c2b229038449241c5e7580ebdf0047576646dbb6
tiffsoa <atiff.soa@gmail.com>
2026-03-11 16:37:12 -0400

    added insights cards in the dashboard

--- src/components/shared/icons.tsx
+++ src/components/shared/icons.tsx
@@ -1,8 +1,12 @@
-import { Eye, Settings, User, TrendingUp } from "lucide-react";
+import { Eye, Settings, User, TrendingUp, Wallet, Target, Flame } from "lucide-react";
 
 export const Icons = {
     eyeLogo: Eye,
     settingsIcon: Settings,
     userIcon: User,
     trendingUpIcon: TrendingUp,
+    walletIcon: Wallet,
+    targetIcon: Target,
+    flameIcon: Flame,
+
 }
--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,11 +1,13 @@
 import Navbar from "@/components/features/navigation/Navbar";
 import { WelcomeBanner } from "@/components/features/welcomeBanner/welcomeBanner";
+import Insights from "@/components/features/insights/Insights";
 
 export default function DashboardPage() {
   return (
     <>
       <Navbar />
       <WelcomeBanner />
+      <Insights />
     </>
   );
 }
